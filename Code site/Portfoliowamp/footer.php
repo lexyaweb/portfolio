c7862c1a5130ec0b48e01c8bde2f52ec55772f1b
alexiafontraille <afontraille@gmail.com>
2020-03-12 14:25:01 +0100

    Create footer.php

---
+++ Code site/Portfoliowamp/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+        <div class="design">
+            <p id="bottom-page">Design Yaelxy copyright</p>
+            <img class="copyright-icon" src="svg/copyright.svg" alt="copyright">
+        </div>
+    </footer>
+</body>
+</html>
