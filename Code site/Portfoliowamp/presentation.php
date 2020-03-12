2bb327553b33056adc27a7fcc56eb6dc61c38bdd
alexiafontraille <afontraille@gmail.com>
2020-03-12 14:25:06 +0100

    Create presentation.php

---
+++ Code site/Portfoliowamp/presentation.php
@@ -0,0 +1,23 @@
+<article>
+        <div class="cadre">
+
+            <div class="presentation-title" id="aboutUs-link">
+                <div class="presentation">
+                    <div class="texte-present">
+                        <div class="toolalign">
+                            <img class="logopresentation" src="svg/logocomp.svg" alt="logo compétence">
+                            <h3>A propos de nous</h3>
+                        </div>
+                        <p>Alexia & Yael HUE, respectivement 26 et 32 ans, nous sommes 2 développeur, en plein apprentissage.
+                        Back end et front end rien ne nous échappe !
+                        Nous sommes originaire du sud de la de la France.</p>
+                    </div>
+                    <div class="image-present">
+                        <img src="Img/yael.png" alt="yael développeur">   
+                        <img src="Img/alexia.png" alt="alexia développeur">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </article>
