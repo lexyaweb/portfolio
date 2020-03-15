b08915347d81901f1692ff8298094e94a62049d5
alexiafontraille <afontraille@gmail.com>
2020-03-15 11:19:06 +0100

    Update traitement_formulaire.php

--- Code site/Portfoliowamp/traitement_formulaire.php
+++ Code site/Portfoliowamp/traitement_formulaire.php
@@ -1,9 +1,11 @@
 <?php
-    /*  ****************************************
-    	    CONFIGURATION
-    	***************************************  */
+    /*
+    	********************************************************************************************
+    	CONFIGURATION
+    	********************************************************************************************
+    */
     // destinataire est votre adresse mail. Pour envoyer à plusieurs à la fois, séparez-les par une virgule
-    $destinataire = 'afontraille@gmail.com';
+    $destinataire = 'moi@moi.tld';
      
     // copie ? (envoie une copie au visiteur)
     $copie = 'oui'; // 'oui' ou 'non'
@@ -13,12 +15,14 @@
     $message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
      
     // Messages d'erreur du formulaire
-    $message_erreur_formulaire = "Vous devez d'abord <a href=\"index.html\">envoyer le formulaire</a>.";
+    $message_erreur_formulaire = "Vous devez d'abord <a href=\"contact.html\">envoyer le formulaire</a>.";
     $message_formulaire_invalide = "Vérifiez que tous les champs soient bien remplis et que l'email soit sans erreur.";
      
-    /* *************************************
-    	    FIN DE LA CONFIGURATION
-       ************************************* */
+    /*
+    	********************************************************************************************
+    	FIN DE LA CONFIGURATION
+    	********************************************************************************************
+    */
      
     // on teste si le formulaire a été soumis
     if (!isset($_POST['envoi']))
@@ -28,25 +32,32 @@
     }
     else
     {
-    	/* cette fonction sert à nettoyer et enregistrer un texte */
-    	function Rec($text){
+    	/*
+    	 * cette fonction sert à nettoyer et enregistrer un texte
+    	 */
+    	function Rec($text)
+    	{
     		$text = htmlspecialchars(trim($text), ENT_QUOTES);
-    		if (1 === get_magic_quotes_gpc()){
+    		if (1 === get_magic_quotes_gpc())
+    		{
     			$text = stripslashes($text);
     		}
+     
     		$text = nl2br($text);
     		return $text;
     	};
      
-    	/* Cette fonction sert à vérifier la syntaxe d'un email */
-    	function IsEmail($email){
+    	/*
+    	 * Cette fonction sert à vérifier la syntaxe d'un email
+    	 */
+    	function IsEmail($email)
+    	{
     		$value = preg_match('/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!\.)){0,61}[a-zA-Z0-9_-]?\.)+[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!$)){0,61}[a-zA-Z0-9_]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/', $email);
     		return (($value === 0) || ($value === false)) ? false : true;
     	}
      
     	// formulaire envoyé, on récupère tous les champs.
-        $nom     = (isset($_POST['nom']))     ? Rec($_POST['nom'])     : '';
-    	$prenom  = (isset($_POST['prenom']))  ? Rec($_POST['prenom'])     : '';
+    	$nom     = (isset($_POST['nom']))     ? Rec($_POST['nom'])     : '';
     	$email   = (isset($_POST['email']))   ? Rec($_POST['email'])   : '';
     	$objet   = (isset($_POST['objet']))   ? Rec($_POST['objet'])   : '';
     	$message = (isset($_POST['message'])) ? Rec($_POST['message']) : '';
@@ -54,7 +65,8 @@
     	// On va vérifier les variables et l'email ...
     	$email = (IsEmail($email)) ? $email : ''; // soit l'email est vide si erroné, soit il vaut l'email entré
      
-    	if (($nom != '') && ($prenom != '') && ($email != '') && ($objet != '') && ($message != '')){
+    	if (($nom != '') && ($email != '') && ($objet != '') && ($message != ''))
+    	{
     		// les 4 variables sont remplies, on génère puis envoie le mail
     		$headers  = 'MIME-Version: 1.0' . "\r\n";
     		$headers .= 'From:'.$nom.' <'.$email.'>' . "\r\n" .
@@ -65,10 +77,12 @@
     				'X-Mailer:PHP/'.phpversion();
      
     		// envoyer une copie au visiteur ?
-    		if ($copie == 'oui'){
+    		if ($copie == 'oui')
+    		{
     			$cible = $destinataire.';'.$email;
     		}
-    		else{
+    		else
+    		{
     			$cible = $destinataire;
     		};
      
@@ -85,19 +99,24 @@
     		// Envoi du mail
     		$num_emails = 0;
     		$tmp = explode(';', $cible);
-    		foreach($tmp as $email_destinataire){
+    		foreach($tmp as $email_destinataire)
+    		{
+                //admin@wampserver.invalid
     			if (mail($email_destinataire, $objet, $message, $headers))
     				$num_emails++;
     		}
      
-    		if ((($copie == 'oui') && ($num_emails == 2)) || (($copie == 'non') && ($num_emails == 1))){
+    		if ((($copie == 'oui') && ($num_emails == 2)) || (($copie == 'non') && ($num_emails == 1)))
+    		{
     			echo '<p>'.$message_envoye.'</p>';
     		}
-    		else{
+    		else
+    		{
     			echo '<p>'.$message_non_envoye.'</p>';
     		};
     	}
-    	else{
+    	else
+    	{
     		// une des 3 variables (ou plus) est vide ...
     		echo '<p>'.$message_formulaire_invalide.' <a href="contact.html">Retour au formulaire</a></p>'."\n";
     	};
