79cd5aee92028381d40f250d04a079e93021b488
yaelxy05 <yaelxy05@gmail.com>
2020-03-19 01:54:52 +0100

    Update script.js

--- Code site/Portfoliowamp/script.js
+++ Code site/Portfoliowamp/script.js
@@ -5,6 +5,26 @@ function smoothScroll(target,duration) {
     let distance = targetPosition - startPosition;
     let startTime = null;
     console.log(startPosition);
+
+    function animationScroll(currentTime){
+        if(startTime === null) startTime = currentTime;
+        let timeElaspsed = currentTime - startTime;
+        let run = ease(timeElaspsed,startPosition,distance,duration);
+        window.scrollTo(0,run);
+        if(timeElaspsed < duration) requestAnimationFrame(animation);
+    }
+    function ease(t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+    requestAnimationFrame(animation);
 }
 
-smoothScroll('#accueil', 1000);
+let accueil = document.querySelector('#accueil');
+
+accueil.addEventListener('click', function()) {
+    smoothScroll('.design', 1000);
+}
+
