648463b4caf5a682220a8dbc62a9a73de7394d1c
alexiafontraille <afontraille@gmail.com>
2020-04-06 15:19:43 +0200

    Create menu.php

---
+++ Code site/Portfoliowamp/menu.php
@@ -0,0 +1,22 @@
+<img src="svg/arrow-up-solid.svg" class="buttonUp">
+    <header>
+        <img class="background" src="Img/mountain.jpg" alt="background 4k">   
+        <div class="header">
+            <nav class="menu">
+                <div class="logo active"><ul>
+                    <li><img class="logo-title" src="svg/Groupe 9.svg"></li>
+                    <li class="title">LEXYA-WEB</li>
+                </ul></div>
+                <ul>
+                    <div id="myLinks">
+                        <li><a href="#" class="Accueil">Accueil</a></li>
+                        <li><a href="#" class="contactlink">Contact</a></li>
+                        <li><a href="#" class="creation-title">Nos créations</a></li>
+                        <li><a href="#" class="aboutUs">A propos de nous</a></li>
+                    </div>
+                    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                        <i class="fa fa-bars"></i>
+                    </a>
+                </ul>
+            </nav>
+        </div>
