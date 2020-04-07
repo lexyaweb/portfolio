ee5e8e47addcec4e342e3aa9fe551d9cb2b49476
alexiafontraille <afontraille@gmail.com>
2020-04-07 16:57:03 +0200

    Update menu.php

--- Code site/Portfoliowamp/menu.php
+++ Code site/Portfoliowamp/menu.php
@@ -3,17 +3,15 @@
         <img class="background" src="Img/mountain.jpg" alt="background 4k">   
         <div class="header">
             <nav class="menu">
-                <div class="logo active"><ul>
+                <ul class="logo active">
                     <li><img class="logo-title" src="svg/Groupe 9.svg"></li>
                     <li class="title">LEXYA-WEB</li>
-                </ul></div>
-                <ul>
-                    <div id="myLinks">
-                        <li><a href="#" class="Accueil">Accueil</a></li>
-                        <li><a href="#" class="contactlink">Contact</a></li>
-                        <li><a href="#" class="creation-title">Nos créations</a></li>
-                        <li><a href="#" class="aboutUs">A propos de nous</a></li>
-                    </div>
+                </ul>
+                <ul id="myLinks">
+                    <li><a href="#" class="Accueil">Accueil</a></li>
+                    <li><a href="#" class="contactlink">Contact</a></li>
+                    <li><a href="#" class="creation-title">Nos créations</a></li>
+                    <li><a href="#" class="aboutUs">A propos de nous</a></li>
                     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                         <i class="fa fa-bars"></i>
                     </a>
