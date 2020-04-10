9c307490bb4fd4e16b50ba4130c51c3506a00035
alexiafontraille <afontraille@gmail.com>
2020-04-10 19:01:27 +0200

    Update menu.php

--- Code site/Portfoliowamp/menu.php
+++ Code site/Portfoliowamp/menu.php
@@ -1,20 +1,39 @@
 <img src="svg/arrow-up-solid.svg" class="buttonUp">
-    <header>
-        <img class="background" src="Img/mountain.jpg" alt="background 4k">   
-        <div class="header">
+<header>
+    <img class="background" src="Img/mountain.jpg" alt="background 4k">   
+    <nav role="navigation" class="menuX">
+        <ul class="logo active">
+            <li><img class="logo-title" src="svg/Groupe 9.svg"></li>
+            <li class="title">LEXYA-WEB</li>
+        </ul>
+        <div id="menuToggle">
+            <input type="checkbox" />
+            <span></span><span></span><span></span>
+            <ul id="menu">
+                <li><a href="#" class="Accueil">Accueil</a></li>
+                <li><a href="#" class="contactlink">Contact</a></li>
+                <li><a href="#" class="creation-title">Nos créations</a></li>
+                <li><a href="#" class="aboutUs">A propos de nous</a></li>
+            </ul>
+        </div>
+    </nav>
+
+<!-- 
             <nav class="menu">
                 <ul class="logo active">
                     <li><img class="logo-title" src="svg/Groupe 9.svg"></li>
                     <li class="title">LEXYA-WEB</li>
                 </ul>
                 <ul id="myLinks">
-                    <li><a href="#" class="Accueil">Accueil</a></li>
-                    <li><a href="#" class="contactlink">Contact</a></li>
-                    <li><a href="#" class="creation-title">Nos créations</a></li>
-                    <li><a href="#" class="aboutUs">A propos de nous</a></li>
+                    <div id="menuInterne">
+                        <li><a href="#" class="Accueil">Accueil</a></li>
+                        <li><a href="#" class="contactlink">Contact</a></li>
+                        <li><a href="#" class="creation-title">Nos créations</a></li>
+                        <li><a href="#" class="aboutUs">A propos de nous</a></li>
+                    </div>
                     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                         <i class="fa fa-bars"></i>
                     </a>
                 </ul>
             </nav>
-        </div>
+        </div> -->
