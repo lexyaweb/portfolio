b10602995b4cf2e51b4b14b6cfb02b181f26c735
yaelxy05 <yaelxy05@gmail.com>
2020-04-15 10:14:54 +0200

    Update index.php

--- Code site/Portfoliowamp/index.php
+++ Code site/Portfoliowamp/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lexya-Web</title>
     <link rel="stylesheet" href="style.css" />
+    <script type="text/javascript" src="script.js"></script> 
 </head>
 <body>
     <?php include ("connect.php"); ?>
@@ -19,20 +20,19 @@
             </div>
         </div>
     </header>
-    <?php
-    require ("skills.php");
-    require ("presentation.php")
-    ?>
+    <?php require ("skills.php"); ?>
+    <?php require ("presentation.php"); ?>
 
     <article>
         <div class="bloc-creation">
             <h3 class="creation" id="creation-link">Nos créations</h3>
         </div>
     </article>
- 
-    <?php 
-    require("contact.php");
-    require("footer.php");
-    ?> 
-    <script type="text/javascript" src="script.js"></script> 
+    
+    <?php require ("contact.php"); ?>
+   
+    <?php require ("footer.php"); ?> 
+
+
+
     
