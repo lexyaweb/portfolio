9a2c01600a57c88c99b6b6e8fed84e001952eb78
yaelxy05 <yaelxy05@gmail.com>
2020-04-20 21:56:37 +0200

    Create app.js

---
+++ entrainement/app.js
@@ -0,0 +1,10 @@
+let a = 17;
+    
+    if (a == 3) {
+        console.log("a vaut 3");
+    }
+    else if (a == 17) {
+        console.log("a vaut 17");
+    } else {
+        console.log("a ne vaut ni 3 ni 17");
+    }
