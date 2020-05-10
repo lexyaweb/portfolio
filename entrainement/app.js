989140f2aa63f879ee1737e64944ded48fde670e
yaelxy05 <yaelxy05@gmail.com>
2020-05-10 14:27:37 +0200

    Update app.js

--- entrainement/app.js
+++ entrainement/app.js
@@ -3,7 +3,7 @@ class Perso {
         this.nom = nom;
         this.classe = classe;
         this.sante = sante;
-        this.puissance = puissance;
+        this.puissance =  puissance;
         this.inteligence = inteligence;
         this.dexterite = dexterite;
         this.xp = 0;
@@ -28,63 +28,41 @@ class Perso {
                console.log(`${this.nom} est mort !!`)
            }
        }
-}
-
-let perso = new Perso ("baldur","sorcier",36,20,15,30);
-let monstre = new Perso("Bilbon la brute","monstre",25,10,12,18)
-perso = 
-perso.bonus() 
-    if(this.bonus >15) {
-        console.log("level up");
+       attaquer(cible) {
+           if(this.sante > 0) {
+               const degats = this.puissance;
+               console.log(`${this.nom} attaque ${cible.nom}
+               et lui inflige ${degats} points de dégats`);
+           cible.sante -= degats;
+           if(cible.sante > 0) {
+               console.log(`${cible.nom} a encore ${cible.sante} points de vie `);
+           } else {
+               cible.sante = 0;
+               const bonusXP = 60;
+               console.log(`${this.nom} a tué ${cible.nom} et gagne ${bonusXP} points d'expérience`);
+               this.xp += bonusXP;
+               if(this.xp > 50) {
+                console.log(`${this.nom} est passé au niveau supérieur`);
+            } else {
+                console.log(`${this.nom} est toujours au même niveau !`)
+            }
+           }
+           
+       } else {
+           console.log(`${this.nom} n'a plus de points de vie et ne peut plus attaquer`);
+       
+        }
     }
+
+}
+let perso = new Perso ("baldur","sorcier",1000,20,15,30);
+let monstre = new Perso("Bilbon la brute","monstre",25,10,12,18);
 
 perso.decrire();
 perso.bonus();
 monstre.decrire();
-
-perso.combat();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*class Moto {
-    constructor(marque,cylindre,prix) {
-        this.marque = marque;
-        this.cylindre = cylindre;
-        this.prix = prix;
-    }
-
-    puissance() {
-        console.log(`cette Moto ${this.marque} a une puissance de ${this.cylindre} CC`)
-    }
-
-    cout() {
-        console.log(`cette moto ${this.marque} coûte ${this.prix} €`)
-    }
-}
-class Car {
-    constructor(m)
-}
-let moto = new Moto("Honda", 900, 1500);
-moto.puissance();
-moto.cout();
-console.log(moto); */
+monstre.attaquer(perso);
+perso.attaquer(monstre);
+perso.attaquer(monstre);
+console.log(perso.decrire());
+console.log(monstre.decrire());
